85d07e88ca55bfc16e34d842f7a8e534c4e0b4a2
danilukk <114990578+danilukk@users.noreply.github.com>
2022-11-07 00:20:13 +0200

    feat: add function to play a single round of the game

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const Shape = {
     Scissors: 'Scissors'
 };
 
+const Winner = {
+    Computer: 'You lose!',
+    Player: 'You won!',
+    Tie: "It's a tie!"
+};
+
 function formatStr(str) {
     if(str === null) return null;
 
@@ -21,4 +27,35 @@ function getPlayerChoice() {
     let playerChoice = formatStr( prompt(formatStr( Object.keys(Shape).join(', ') )  + '?') );
 
     return playerChoice;
+}
+
+function playRound() {
+    const computerChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+
+    return {
+        playerChoice: playerChoice,
+        computerChoice: computerChoice,
+        additionalMessage: (
+                            playerChoice != computerChoice
+                            && (playerChoice === Shape.Rock || playerChoice === Shape.Scisors)
+                            && (computerChoice === Shape.Rock || computerChoice === Shape.Scissors)
+                            ) ? 'Rock beats scissors!'
+                        : (
+                            playerChoice != computerChoice
+                            && (playerChoice === Shape.Paper || playerChoice === Shape.Scisors)
+                            && (computerChoice === Shape.Paper || computerChoice === Shape.Scissors)
+                            ) ? 'Scissors beats paper!'
+                        : (
+                            playerChoice != computerChoice
+                            && (playerChoice === Shape.Paper || playerChoice === Shape.Rock)
+                            && (computerChoice === Shape.Paper || computerChoice === Shape.Rock)
+                            ) ? 'Paper beats rock!'
+                        : '',
+        winner: (playerChoice === computerChoice) ? Winner.Tie
+                : (playerChoice === Shape.Rock && computerChoice === Shape.Scissors) ? Winner.Player
+                : (playerChoice === Shape.Scissors && computerChoice === Shape.Paper) ? Winner.Player
+                : (playerChoice === Shape.Paper && computerChoice === Shape.Rock) ? Winner.Player
+                : Winner.Computer
+    };
 }
