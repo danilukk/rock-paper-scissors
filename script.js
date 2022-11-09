c1c5107ac8a52bbba9ead9e67956ecfe0e4c692e
danilukk <114990578+danilukk@users.noreply.github.com>
2022-11-09 15:06:27 +0200

    feat: add function to format a string

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const Shape = {
     Scissors: 'Scissors'
 };
 
+function formatStr(str) {
+    if(str === null) return null;
+
+    // '   sTrInG' => 'String'
+    str = str.trim().split('');
+    return str.shift().toUpperCase() + str.join('').toLowerCase();
+}
+
 function getComputerChoice() {
     // Returns random Shape
     return Object.keys(Shape)[Math.floor(Math.random() * Object.keys(Shape).length)];
